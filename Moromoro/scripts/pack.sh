37241d0bc90bef0a5509325caa0ebc6b4e30ac07
Goh Wei Wen <goweiwen@gmail.com>
2023-02-27 16:14:25 +0800

    Fix resource pack for 1.19.3

--- Moromoro/scripts/pack.sh
+++ Moromoro/scripts/pack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-scp weiwen@ssh.piggyp.ink:/opt/nomad/volumes/blanktopia/plugins/Moromoro/pack.zip .
+# scp weiwen@ssh.piggyp.ink:/opt/nomad/volumes/blanktopia/plugins/Moromoro/pack.zip .
 
 HASH=$(shasum pack.zip | cut -d' ' -f1)
 
@@ -8,4 +8,7 @@ mv pack.zip dist/Blanktopia-$HASH.zip
 rsync -iavzhP dist/ weiwen@ssh.piggyp.ink:/opt/nomad/volumes/files/ --rsync-path="sudo rsync"
 
 echo $HASH
-yq -y ".[\"resource-pack-hash\"] = \"$HASH\" | .[\"resource-pack-url\"] = \"http://files.blanktopia.com/Blanktopia-$HASH.zip\"" config.yml | sponge config.yml
+
+scp weiwen@ssh.piggyp.ink:/opt/nomad/volumes/blanktopia/plugins/Moromoro/config.yml dist/config.yml
+sed -i -e "s/^resource-pack-url: .*\$/resource-pack-url: http:\/\/files.blanktopia.com\/Blanktopia-$HASH.zip/" -e "s/^resource-pack-hash: .*\$/resource-pack-hash: $HASH/" dist/config.yml
+scp dist/config.yml weiwen@ssh.piggyp.ink:/opt/nomad/volumes/blanktopia/plugins/Moromoro/config.yml
