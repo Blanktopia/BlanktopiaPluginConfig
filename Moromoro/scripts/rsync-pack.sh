40b9700afe07a629e3a7a264d52a837f9c633367
Wei Wen Goh <weiwen.goh@bytedance.com>
2025-03-03 17:12:50 +0800

    Update datapack

--- Moromoro/scripts/rsync-pack.sh
+++ Moromoro/scripts/rsync-pack.sh
@@ -2,5 +2,5 @@
 
 hash=$(sha1sum pack.zip | cut -d' ' -f1)
 rsync pack.zip weiwen@blanktopia.com:~/Docker/caddy/files/Blanktopia-$hash.zip
-yq ".[\"resource-pack-url\"] = \"https://files.blanktopia.com/Blanktopia-$hash.zip\"" -iY config.yml
-yq ".[\"resource-pack-hash\"] = \"$hash\"" -iY config.yml
+yq -y -i ".[\"resource-pack-url\"] = \"https://files.blanktopia.com/Blanktopia-$hash.zip\"" config.yml
+yq -y -i ".[\"resource-pack-hash\"] = \"$hash\"" config.yml
