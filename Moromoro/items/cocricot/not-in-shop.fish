1bbe9aaf1a31a84c191a89e8af3b26e2a949a266
Goh Wei Wen <goweiwen@gmail.com>
2025-03-15 16:37:45 +0800

    Add missing items to ikea

--- Moromoro/items/cocricot/not-in-shop.fish
+++ Moromoro/items/cocricot/not-in-shop.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 
 find {bed,chair,nonsolid,solid,wall} -name '*.yml' -exec sh -c "basename {} | sed 's/\.yml\$//'" \; | sort -u > /tmp/all.txt
-yq '.categories | to_entries | map(.value) | flatten(1) | map(.item) | .[]' ../../shops/ikea.yml | sort -u > /tmp/shop.txt
+yq -r '.categories | to_entries | map(.value) | flatten(1) | map(.item) | .[]' ../../shops/ikea.yml | sort -u > /tmp/shop.txt
 
 comm -23 /tmp/all.txt /tmp/shop.txt | grep -vE -e "-(both|corner|side|all|three|left|right|inner|outer)"
